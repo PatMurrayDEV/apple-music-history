68be62d1fa6968afe0b40470468575f78a3ad5f0
Patrick Murray <contact@patmurraydev.com>
2018-12-12 10:34:56 +1100

    added sentry

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+    dsn: "https://8435d5183ce3450ba6dd60acfe70080a@sentry.io/1342325"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
