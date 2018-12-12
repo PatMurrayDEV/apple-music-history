e227fb60c33ca73f99972f3afb169e8d62bf709d
Patrick Murray <contact@patmurraydev.com>
2018-12-12 18:35:30 +1100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ class Footer extends Component {
     render(){
         return(
             <div className="footer">
-                Created by <a href="https://patmurray.co">Pat Murray</a>. Apple and Apple Music are trademarks of Apple Inc., registered in the U.S. and other countries. 
+                Created by <a href="https://patmurray.co">Pat Murray</a>. <a href="https://www.paypal.me/patmurray/5">Tips are appreciated!</a>. Apple and Apple Music are trademarks of Apple Inc., registered in the U.S. and other countries. 
             </div>
         )
     }
