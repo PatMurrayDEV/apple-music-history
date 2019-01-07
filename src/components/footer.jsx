6aa105f3927b266d8ec880b578564871997afb3e
Patrick Murray <contact@patmurraydev.com>
2019-01-07 15:39:43 +1100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ class Footer extends Component {
     render(){
         return(
             <div className="footer">
-                Created by <a href="https://patmurray.co">Pat Murray</a>. <a href="https://www.paypal.me/patmurray/5">Tips are appreciated!</a>. If you like this, please <a href="https://itunes.apple.com/au/app/atlas-wallpaper/id1368484008?mt=8">check out my apps</a>. <br/>Apple and Apple Music are trademarks of Apple Inc., registered in the U.S. and other countries. 
+                Created by <a href="https://patmurray.co">Pat Murray</a>. <a href="https://patmurray.co/tip/">Tips are appreciated!</a>. If you like this, please <a href="https://itunes.apple.com/au/app/atlas-wallpaper/id1368484008?mt=8">check out my apps</a>. <br/>Apple and Apple Music are trademarks of Apple Inc., registered in the U.S. and other countries. 
             </div>
         )
     }
