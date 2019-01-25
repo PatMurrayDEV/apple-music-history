86bbe83dd13e3a0462d2cfd608b58f41fb0b7920
Patrick Murray <contact@patmurraydev.com>
2019-01-25 16:24:04 +1100

    fix heatmap

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -130,7 +130,7 @@ class Results extends Component {
 
         const xLabels = ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'];
         const xLabelsVisibility = [true, false, false, true, false, false, true, false, false, true, false, false, true, false, false, true, false, false, true, false, false, true, false, false]
-        const yLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+        const yLabels = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
 
 
 
